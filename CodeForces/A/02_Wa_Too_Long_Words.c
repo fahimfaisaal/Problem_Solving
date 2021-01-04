9b297573153e73f6d6f8ef0749eb66b22fa0e9ca
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-01-04 18:20:16 +0600

    add solution of string task & add solution of wa too long words with c

--- CodeForces/A/02_Wa_Too_Long_Words.c
+++ CodeForces/A/02_Wa_Too_Long_Words.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-//! Didn't solve with c
+
     int count = 1;
-    char word[] = "internationalization";
+    char word[100];
 
     scanf("%d", &count);
 
     if (count >= 1 && count <= 100) while(count--) {
         scanf("%s", &word);
-        int length = sizeof(word) - 1;
+        int length = strlen(word);
 
         if (length > 10) printf("%c%d%c\n", word[0], length - 2, word[length - 1]);        
         else printf("%s\n", word);
---
+++ CodeForces/A/05_String_Task.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+bool is_vowel(char character) {
+
+    char vowels[6] = {'A', 'E', 'I', 'O', 'U', 'Y'};
+
+    for (int i = 0; i < 6; i++) {
+       if (toupper(character) == vowels[i])
+           return true;
+    }
+
+    return false;
+}
+
+int main() {
+
+    char word[100];
+
+    scanf("%[^\n]%*c", word);
+
+    int length = strlen(word);
+
+    if (length > 0 && length <= 100) {
+
+        for (int i = 0; i < length; i++) {
+            if (is_vowel(word[i])) continue;
+            else printf(".%c", tolower(word[i]));    
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
