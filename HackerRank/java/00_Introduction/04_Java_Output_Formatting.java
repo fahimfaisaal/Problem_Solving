d0f3d4671079544572fc51e7d8f8c6e6a1a70ea4
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-03-10 15:03:38 +0600

    add java Solutions from hackerrank

---
+++ HackerRank/java/00_Introduction/04_Java_Output_Formatting.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("================================");
+            
+            for (int i = 0; i < 3; i++) {
+                String s1 = sc.next();
+                int x = sc.nextInt();
+                
+                System.out.print(s1);
+                
+                for (int j = 0; j < 15 - s1.length(); j++) {
+                    System.out.print(" ");
+                }
+                
+                System.out.println((x < 10 ? "00" : x < 100 ? "0" : "") + x);
+
+                // System.out.printf("%-15s%03d%n", s1, x); (also can use this line only)
+            }
+            
+            System.out.println("================================");
+    }
+}
