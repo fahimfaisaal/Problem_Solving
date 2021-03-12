2c39068b29f633a4146617202422eef531d6d4c9
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-03-12 21:20:05 +0600

    add one more solution

---
+++ HackerRank/java/00_Introduction/06_java_Loops_II.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static int pow(int base, int pow) {
+        if (pow == 0) return 1;
+        
+        return base * pow(base, pow - 1);
+    }
+    
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int t = in.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            for (int j = 0; j < n; j++) {
+                a += (Solution.pow(2, j) * b);
+                System.out.print(a + " ");
+            }
+            
+            System.out.print("\n");
+        }
+        
+        in.close();
+    }
+}
